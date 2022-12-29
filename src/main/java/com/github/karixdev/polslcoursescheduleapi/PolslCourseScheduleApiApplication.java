8332f28c33aac346afdc97225d03b92a5b3c6827
karixdev <30869888+karixdev@users.noreply.github.com>
2022-12-29 17:25:00 +0100

    init

---
+++ src/main/java/com/github/karixdev/polslcoursescheduleapi/PolslCourseScheduleApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.karixdev.polslcoursescheduleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolslCourseScheduleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolslCourseScheduleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/karixdev/polslcoursescheduleapi/PolslCourseScheduleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.karixdev.polslcoursescheduleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolslCourseScheduleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
