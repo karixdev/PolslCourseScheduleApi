b0222e9a8e537d2872ee3c0774e7a9d6a3b4dcdf
karixdev <30869888+karixdev@users.noreply.github.com>
2022-12-29 18:09:36 +0100

    feat: create test containers environment class

---
+++ src/test/java/com/github/karixdev/polslcoursescheduleapi/ContainersEnvironment.java
@@ -0,0 +1,38 @@
+package com.github.karixdev.polslcoursescheduleapi;
+
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@ActiveProfiles("test")
+public class ContainersEnvironment {
+    @Container
+    private static final MySQLContainer<?> mySQLContainer =
+            new MySQLContainer<>("mysql:latest")
+                    .withDatabaseName("rating-youtube-thumbnails-test")
+                    .withUsername("root")
+                    .withPassword("root");
+
+    @DynamicPropertySource
+    static void overrideYoutubeApiBaseUrl(DynamicPropertyRegistry dynamicPropertyRegistry) {
+        dynamicPropertyRegistry.add(
+                "spring.datasource.url",
+                mySQLContainer::getJdbcUrl);
+
+        dynamicPropertyRegistry.add(
+                "spring.datasource.username",
+                mySQLContainer::getUsername);
+
+        dynamicPropertyRegistry.add(
+                "spring.datasource.password",
+                mySQLContainer::getPassword);
+
+        dynamicPropertyRegistry.add(
+                "spring.datasource.driver-class-name",
+                mySQLContainer::getDriverClassName);
+    }
+}
