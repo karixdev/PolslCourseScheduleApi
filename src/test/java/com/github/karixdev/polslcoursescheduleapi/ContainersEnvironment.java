cb7d75c4a5680d3cc98f3320dca69520b0b81a1a
karixdev <30869888+karixdev@users.noreply.github.com>
2023-01-03 01:14:00 +0100

    feat: use reuse test containers

--- src/test/java/com/github/karixdev/polslcoursescheduleapi/ContainersEnvironment.java
+++ src/test/java/com/github/karixdev/polslcoursescheduleapi/ContainersEnvironment.java
@@ -1,21 +1,31 @@
 package com.github.karixdev.polslcoursescheduleapi;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.Duration;
 
 @Testcontainers
 @ActiveProfiles("test")
 public class ContainersEnvironment {
-    @Container
-    private static final MySQLContainer<?> mySQLContainer =
+    static final MySQLContainer<?> mySQLContainer =
             new MySQLContainer<>("mysql:latest")
                     .withDatabaseName("rating-youtube-thumbnails-test")
                     .withUsername("root")
-                    .withPassword("root");
+                    .withPassword("root")
+                    .withReuse(true);
+
+    @BeforeAll
+    static void beforeAll() {
+        mySQLContainer.start();
+    }
 
     @DynamicPropertySource
     static void overrideDatabaseConnectionProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
