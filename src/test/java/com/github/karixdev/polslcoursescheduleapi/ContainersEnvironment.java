b3617160b33a169729724728da25b31585ddb94b
karixdev <30869888+karixdev@users.noreply.github.com>
2023-01-03 01:14:31 +0100

    fix: remove postgres imports

--- src/test/java/com/github/karixdev/polslcoursescheduleapi/ContainersEnvironment.java
+++ src/test/java/com/github/karixdev/polslcoursescheduleapi/ContainersEnvironment.java
@@ -5,12 +5,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-
-import java.time.Duration;
 
 @Testcontainers
 @ActiveProfiles("test")
