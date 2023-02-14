ad33c044b65a006c71f27c225e4d52cabc4b0ea6
karixdev <kari.klucz@gmail.com>
2023-02-14 19:01:15 +0100

    feat: create schedule dtos

---
+++ schedule-service/src/main/java/com/github/karixdev/scheduleservice/dto/ScheduleRequest.java
@@ -0,0 +1,29 @@
+package com.github.karixdev.scheduleservice.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+
+public record ScheduleRequest(
+        @JsonProperty("type")
+        @NotNull
+        @PositiveOrZero
+        Integer type,
+        @JsonProperty("plan_polsl_id")
+        @NotNull
+        @Positive
+        Integer planPolslId,
+        @JsonProperty("semester")
+        @NotNull
+        @Positive
+        Integer semester,
+        @JsonProperty("name")
+        @NotBlank
+        String name,
+        @JsonProperty("group_number")
+        @NotNull
+        @Positive
+        Integer groupNumber
+) {}
