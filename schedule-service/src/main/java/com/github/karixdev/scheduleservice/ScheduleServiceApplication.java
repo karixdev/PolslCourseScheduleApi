1ef1163c397c3ac85fac2d6c29135941263c4904
karixdev <kari.klucz@gmail.com>
2023-02-14 18:28:16 +0100

    fix: remove typo from main class

--- schedule-service/src/main/java/com/github/karixdev/scheduleservice/ScheduleServiceApplication.java
+++ schedule-service/src/main/java/com/github/karixdev/scheduleservice/ScheduleServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScheduleserviceApplication {
+public class ScheduleServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ScheduleserviceApplication.class, args);
+		SpringApplication.run(ScheduleServiceApplication.class, args);
 	}
 
 }
