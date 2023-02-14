5a84425d9e400103dd788c9886bf4c863817aedd
karixdev <kari.klucz@gmail.com>
2023-02-14 18:14:22 +0100

    feat: create schedule service app

---
+++ schedule-service/src/main/java/com/github/karixdev/scheduleservice/ScheduleserviceApplication.java
@@ -0,0 +1,13 @@
+package com.github.karixdev.scheduleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleserviceApplication.class, args);
+	}
+
+}
