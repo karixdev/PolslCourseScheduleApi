90f80bf6adbc1e01f156ee13ef2e290b2cdb5763
karixdev <kari.klucz@gmail.com>
2023-02-17 19:45:15 +0100

    feat: change schedule service db to postgresql

--- schedule-service/src/test/java/com/github/karixdev/scheduleservice/ContainersEnvironment.java
+++ schedule-service/src/test/java/com/github/karixdev/scheduleservice/ContainersEnvironment.java
@@ -4,40 +4,40 @@ import org.junit.jupiter.api.BeforeAll;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 @ActiveProfiles("test")
 public class ContainersEnvironment {
-    static final MySQLContainer<?> mySQLContainer =
-            new MySQLContainer<>("mysql:latest")
-                    .withDatabaseName("schedule-service-test")
+    static final PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres:15.1-alpine")
                     .withUsername("root")
                     .withPassword("root")
+                    .withDatabaseName("schedule-service-test")
                     .withReuse(true);
 
     @BeforeAll
     static void beforeAll() {
-        mySQLContainer.start();
+        postgreSQLContainer.start();
     }
 
     @DynamicPropertySource
     static void overrideDatabaseConnectionProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
         dynamicPropertyRegistry.add(
                 "spring.datasource.url",
-                mySQLContainer::getJdbcUrl);
+                postgreSQLContainer::getJdbcUrl);
 
         dynamicPropertyRegistry.add(
                 "spring.datasource.username",
-                mySQLContainer::getUsername);
+                postgreSQLContainer::getUsername);
 
         dynamicPropertyRegistry.add(
                 "spring.datasource.password",
-                mySQLContainer::getPassword);
+                postgreSQLContainer::getPassword);
 
         dynamicPropertyRegistry.add(
                 "spring.datasource.driver-class-name",
-                mySQLContainer::getDriverClassName);
+                postgreSQLContainer::getDriverClassName);
     }
 }
