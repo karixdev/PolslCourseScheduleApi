8f46622c6d685276a027afbdb4484c90f723b76a
karixdev <kari.klucz@gmail.com>
2023-02-25 21:07:54 +0100

    feat: create web scraper service

---
+++ web-scraper-service/src/main/java/com/github/karixdev/webscraperservice/WebScraperServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.karixdev.webscraperservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebScraperServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebScraperServiceApplication.class, args);
+	}
+
+}
