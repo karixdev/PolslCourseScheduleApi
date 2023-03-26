5b7a575dcaa3cbb438ff17709e1bc866cc616ec9
karixdev <kari.klucz@gmail.com>
2023-03-26 21:25:31 +0200

    feat: change course mq message model

--- schedule-service/src/main/java/com/github/karixdev/scheduleservice/course/CourseMapper.java
+++ schedule-service/src/main/java/com/github/karixdev/scheduleservice/course/CourseMapper.java
@@ -1,26 +1,24 @@
 package com.github.karixdev.scheduleservice.course;
 
-import com.github.karixdev.scheduleservice.course.message.CourseMessage;
+import com.github.karixdev.scheduleservice.course.dto.BaseCourseDTO;
 import com.github.karixdev.scheduleservice.schedule.Schedule;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CourseMapper {
-    public Course map(CourseMessage message, Schedule schedule) {
-        String teachers = String.join(", ", message.teachers());
-        String rooms = String.join(", ", message.rooms());
 
+    public Course map(BaseCourseDTO courseDTO, Schedule schedule) {
         return Course.builder()
-                .name(message.name())
-                .courseType(message.courseType())
-                .additionalInfo(message.additionalInfo())
-                .dayOfWeek(message.dayOfWeek())
-                .startsAt(message.startsAt())
-                .endsAt(message.endsAt())
-                .teachers(teachers)
-                .classroom(rooms)
+                .name(courseDTO.getName())
+                .courseType(courseDTO.getCourseType())
+                .additionalInfo(courseDTO.getAdditionalInfo())
+                .dayOfWeek(courseDTO.getDayOfWeek())
+                .startsAt(courseDTO.getStartsAt())
+                .endsAt(courseDTO.getEndsAt())
+                .teachers(courseDTO.getTeachers())
+                .classroom(courseDTO.getClassrooms())
                 .schedule(schedule)
-                .weekType(message.weeks())
+                .weekType(courseDTO.getWeekType())
                 .build();
     }
 }
