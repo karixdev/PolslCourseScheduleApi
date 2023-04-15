ad0e15ec0b320992fed88bae8d2b8a6fe7121280
karixdev <kari.klucz@gmail.com>
2023-04-15 22:13:46 +0200

    feat: create discord-webbhooks app

---
+++ discord-webhooks-service/src/main/java/com/example/discordwebhooksservice/DiscordWebhooksServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.discordwebhooksservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordWebhooksServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscordWebhooksServiceApplication.class, args);
+	}
+
+}
---
+++ discord-webhooks-service/src/test/java/com/example/discordwebhooksservice/DiscordWebhooksServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.discordwebhooksservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordWebhooksServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
