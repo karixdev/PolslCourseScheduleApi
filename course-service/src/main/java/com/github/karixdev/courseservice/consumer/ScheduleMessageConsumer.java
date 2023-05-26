20ac675b6ee30bb7040e899e6b9c7de698e89cba
karixdev <kari.klucz@gmail.com>
2023-05-26 23:20:11 +0200

    feat: delete courses when schedule deletion event is consumed

--- course-service/src/main/java/com/github/karixdev/courseservice/consumer/ScheduleMessageConsumer.java
+++ course-service/src/main/java/com/github/karixdev/courseservice/consumer/ScheduleMessageConsumer.java
@@ -1,17 +1,20 @@
 package com.github.karixdev.courseservice.consumer;
 
 import com.github.karixdev.courseservice.message.ScheduleEventMessage;
+import com.github.karixdev.courseservice.props.ScheduleMQProperties;
 import com.github.karixdev.courseservice.service.CourseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
+import static com.github.karixdev.courseservice.props.ScheduleMQProperties.SCHEDULE_UPDATE_QUEUE;
 
 @Component
 @RequiredArgsConstructor
 public class ScheduleMessageConsumer {
     private final CourseService courseService;
 
-    @RabbitListener(queues = "schedule.delete")
+    @RabbitListener(queues = SCHEDULE_UPDATE_QUEUE)
     private void listenForScheduleDeletion(ScheduleEventMessage message) {
         courseService.handleScheduleDelete(message.id());
     }
