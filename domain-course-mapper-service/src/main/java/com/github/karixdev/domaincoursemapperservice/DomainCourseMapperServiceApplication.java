38a8fc56df9eb6f868a661311cd3b11175ffa8f6
karixdev <kari.klucz@gmail.com>
2023-05-28 01:05:30 +0200

    feat: init domain course mapper

---
+++ domain-course-mapper-service/src/main/java/com/github/karixdev/domaincoursemapperservice/DomainCourseMapperServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.karixdev.domaincoursemapperservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainCourseMapperServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainCourseMapperServiceApplication.class, args);
+	}
+
+}
---
+++ domain-course-mapper-service/src/test/java/com/github/karixdev/domaincoursemapperservice/DomainCourseMapperServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.karixdev.domaincoursemapperservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainCourseMapperServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
