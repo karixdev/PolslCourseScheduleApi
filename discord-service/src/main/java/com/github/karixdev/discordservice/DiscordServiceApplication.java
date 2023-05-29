1260c2bef619d11dfc2a446159fa190dfd491d19
karixdev <kari.klucz@gmail.com>
2023-05-29 19:44:38 +0200

    feat: init discord service

---
+++ discord-service/src/main/java/com/github/karixdev/discordservice/DiscordServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.karixdev.discordservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscordServiceApplication.class, args);
+	}
+
+}
---
+++ discord-service/src/test/java/com/github/karixdev/discordservice/DiscordServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.karixdev.discordservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
