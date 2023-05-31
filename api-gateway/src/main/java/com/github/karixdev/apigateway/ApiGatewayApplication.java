0bdda2100196a3b2471607e0a035164da339bd9f
karixdev <kari.klucz@gmail.com>
2023-05-31 12:41:13 +0200

    feat: config swagger in api gateway

--- api-gateway/src/main/java/com/github/karixdev/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/github/karixdev/apigateway/ApiGatewayApplication.java
@@ -2,12 +2,27 @@ package com.github.karixdev.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	RouteLocator routeLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(r -> r.path("/api/schedules/**").uri("lb://schedule-service"))
+				.route(r -> r.path("/api/webhooks/**").uri("lb://webhook-service"))
+				.route(r -> r.path("/api/course-service/**").uri("lb://course-service"))
+				.route(r -> r.path("/api/notifications/**").uri("lb://notification-service"))
+
+				.route(r -> r.path("/course-service/v3/api-docs").uri("lb://course-service"))
+				.build();
 	}
 
 }
