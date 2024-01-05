b15937c3491091f1b4be93198a676a52eb1184ca
karixdev <kari.klucz@gmail.com>
2024-01-05 20:46:06 +0100

    refactor: remove old webhook-service and notification-service

--- notification-service/src/main/java/com/github/karixdev/notificationservice/props/NotificationProperties.java
+++
@@ -1,5 +0,0 @@
-package com.github.karixdev.notificationservice.props;
-
-public class NotificationProperties {
-    public static final String WELCOME_MESSAGE = "Welcome to PolslCourseScheduleApi!";
-}
--- webhook-service/src/main/java/com/github/karixdev/webhookservice/props/ScheduleMQProperties.java
+++
@@ -1,7 +0,0 @@
-package com.github.karixdev.webhookservice.props;
-
-public class ScheduleMQProperties {
-    public static final String SCHEDULE_TOPIC = "schedule_updates";
-    public static final String SCHEDULE_UPDATE_ROUTING_KEY = "schedule.update.notification";
-    public static final String SCHEDULE_UPDATE_QUEUE = "schedule.update.notification";
-}
