4cf821961a8f4403b438956f3eb4970025c89597
karixdev <kari.klucz@gmail.com>
2024-03-25 17:46:09 +0100

    feat: implement rest exception handlers

---
+++ schedule-service/src/main/java/com/github/karixdev/scheduleservice/infrastructure/rest/exception/handler/payload/ValidationErrorResponse.java
@@ -0,0 +1,26 @@
+package com.github.karixdev.scheduleservice.infrastructure.rest.exception.handler.payload;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.http.HttpStatus;
+
+import java.util.Map;
+
+public record ValidationErrorResponse(
+        @JsonProperty("constraints")
+        Map<String, String> constraints,
+        @JsonProperty("status")
+        Integer status,
+        @JsonProperty("error")
+        String error,
+        @JsonProperty("message")
+        String message
+) {
+    public ValidationErrorResponse(Map<String, String> constraints) {
+        this(
+                constraints,
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.toString(),
+                "Validation Failed"
+        );
+    }
+}
