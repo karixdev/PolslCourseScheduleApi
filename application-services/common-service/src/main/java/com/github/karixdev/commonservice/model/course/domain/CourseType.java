9c3cde780e9f4130e5f9138f177eb0e6b473be8c
karixdev <kari.klucz@gmail.com>
2024-04-15 01:47:47 +0200

    refactor: remove common-service

--- application-services/common-service/src/main/java/com/github/karixdev/commonservice/model/course/domain/CourseType.java
+++
@@ -1,9 +0,0 @@
-package com.github.karixdev.commonservice.model.course.domain;
-
-public enum CourseType {
-    LECTURE,
-    LAB,
-    PROJECT,
-    PRACTICAL,
-    INFO
-}
--- application-services/common-service/src/main/java/com/github/karixdev/commonservice/model/schedule/raw/TimeCell.java
+++
@@ -1,8 +0,0 @@
-package com.github.karixdev.commonservice.model.schedule.raw;
-
-import lombok.Builder;
-
-@Builder
-public record TimeCell(
-        String text
-) {}
