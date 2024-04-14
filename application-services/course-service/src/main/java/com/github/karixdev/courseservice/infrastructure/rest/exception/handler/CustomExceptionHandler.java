eb12c225272838db0ae679616baf46377f55ce9a
karixdev <kari.klucz@gmail.com>
2024-04-15 01:45:59 +0200

    feat: add prefab exception handler

--- application-services/course-service/src/main/java/com/github/karixdev/courseservice/infrastructure/rest/exception/handler/CustomExceptionHandler.java
+++
@@ -1,7 +0,0 @@
-package com.github.karixdev.courseservice.infrastructure.rest.exception.handler;
-
-import com.github.karixdev.commonservice.exception.handler.CustomExceptionHandlerBase;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
-public class CustomExceptionHandler extends CustomExceptionHandlerBase {}
